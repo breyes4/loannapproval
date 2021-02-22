bbbfeac2f1c16a0c0f2fc1db17fc87bf989dda34
breyes4 <71798440+breyes4@users.noreply.github.com>
2021-02-21 23:59:35 -0600

    Create code.js

---
+++ code.js
@@ -0,0 +1,92 @@
+$(document).ready(
+    function () {
+
+        var loanRules = {
+            salary:
+                {
+                    required: true,
+                    digits: true,
+                    min: 0,
+                    max: 999999999
+                },
+
+            credit: {
+                required: true,
+                digits: true,
+                min: 300,
+                max: 850
+            },
+
+            months: {
+                required: true,
+                digits: true,
+                min: 0,
+                max: 840
+            }
+        }
+
+        $("form").validate(
+            {
+                submitHandler: processLoan,
+                rules: loanRules,
+                messages: loanMessages,
+            }
+        );
+
+        var loanMessages =
+            {
+                salary: {
+                    required: "Salary is required.",
+                    digits: "Salary must be a number.",
+                    min: "Salary must be greater than or equal to zero.",
+                    max: "Salary must be below 1000000000."
+                },
+
+                credit: {
+                    required: "Credit score is required.",
+                    digits: "Credit score must be a number.",
+                    min: "Credit score must be greater than or equal to 300.",
+                    max: "Salary must be less than or equal to 850."
+                },
+
+                months: {
+                    required: "Months are required.",
+                    digits: "Months must be a number.",
+                    min: "Months must be greater than or equal to zero.",
+                    max: "Months must be less than or equal to 840."
+                }
+            }
+
+
+        function processLoan() {
+            $("form").submit(processLoan);
+            var months = parseFloat($("#months").val());
+            var salary = parseFloat($("#salary").val());
+            var credit = parseFloat($("#credit").val());
+
+
+            if (salary < 40000 && credit < 600)
+            {
+                $("#results").text("Loan denied");
+            }
+
+            else if (salary < 40000 && credit >= 600 && months <= 12)
+            {
+                $("#results").text("Loan denied");
+            }
+
+            else if (salary < 40000 && credit >= 600 && months > 12)
+            {
+                $("#results").text("Loan approved");
+            }
+
+            else if (salary >= 40000 && credit <= 600 && months <= 12) {
+                $("#results").text("Loan denied");
+            }
+
+            else
+            {
+                $("#results").text("Loan approved");
+            }
+        }
+    });
